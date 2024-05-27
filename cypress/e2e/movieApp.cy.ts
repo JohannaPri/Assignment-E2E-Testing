9e231cdc12fd8b02923c39130b593d78698a0555
Johanna Prinz <Johannaa.L@hotmail.com>
2024-05-27 13:51:02 +0200

    ✅ Added more tests that passed

--- cypress/e2e/movieApp.cy.ts
+++ cypress/e2e/movieApp.cy.ts
@@ -1,4 +1,4 @@
-import { movieSort } from './../../src/ts/functions';
+import { movieSort } from '../../src/ts/functions';
 
 beforeEach(() => {
   cy.visit('/');
@@ -46,14 +46,17 @@ describe("mock data tests", () => {
     cy.intercept("GET", "http://omdbapi.com/?apikey=416ed51a&s=*", {
       fixture: "noResponse",
     }).as("omdbCall");
+    
     cy.get("#searchText").type(" ");
-
     cy.get("#search").click();
-
+    
     cy.wait("@omdbCall");
-    cy.get("#movie-container").should("not.contain", "Avatar");
-    cy.get("movie").should("have.length", 0);
-    cy.get("p").should("exist").should("contain", "Inga");
+    
+    // Kontrollera att inga filmer visas
+    cy.get(".movie").should("have.length", 0);
+    
+    // Kontrollera att felmeddelandet visas
+    cy.get("p").should("exist").should("contain", "Inga sökresultat att visa");
   });
 
   it("should render mock data to html", () => {
@@ -82,6 +85,22 @@ describe('api data tests', () => {
 
     cy.get('#movie-container').should('contain', 'Avatar');
   });
+
+  it('should not get api data with incorrect url', () => {
+    cy.intercept("GET", "http://omdbapi.com/?apikey=416ed51a&s=*", {
+      statusCode: 404,
+      body: {},
+      delayMs: 1000,
+    }).as("omdbCall");
+    
+    cy.get('#searchText').type(' ');
+    cy.get('#search').click();
+    
+    cy.wait("@omdbCall");
+    
+    // Kontrollera att inga filmer visas
+    cy.get('.movie').should('not.exist');
+  });
 });  
 
 describe("Sorting functionality tests", () => {
@@ -103,5 +122,21 @@ describe("Sorting functionality tests", () => {
         expect(sortedMovies[i].Title < sortedMovies[i - 1].Title).to.be.true;
       }
     });
+  });
+
+  it("should handle sorting an empty list of movies", () => {
+    const emptyList = [];
+    const sortedMovies = movieSort(emptyList);
+    // Verifiera att en tom lista returneras oförändrad
+    expect(sortedMovies).to.deep.equal(emptyList);
+  });
+
+  it("should handle sorting a list with only one movie", () => {
+    const singleMovieList = [
+      { Title: "Avatar", Year: "2009", imdbID: "tt0499549", Type: "movie", Poster: "https://m.media-amazon.com/images/M/MV5BZDA0OGQxNTItMDZkMC00N2UyLTg3MzMtYTJmNjg3Nzk5MzRiXkEyXkFqcGdeQXVyMjUzOTY1NTc@._V1_SX300.jpg" }
+    ];
+    const sortedMovies = movieSort(singleMovieList);
+    // Verifiera att listan med en enda film returneras oförändrad
+    expect(sortedMovies).to.deep.equal(singleMovieList);
   });
 });
